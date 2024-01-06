6d74e1c1a7ac24319b47bf4f8579a0327870c182
rrsr28 <rr.sanjayram@gmail.com>
2024-01-06 16:03:51 +0530

    Final level1a

--- Level1.py
+++ Level1.py
@@ -1,39 +1,38 @@
 # Alumni Hosted Hackathon 2024
 # Author - Sanjay Ram R R (21PD32)
 
+import json
+import itertools
+
 # Import statements
 # ----------------
 
-import json
+file_path = 'Input data\\level1a.json'
+with open(file_path) as file:
+    data = json.load(file)
 
-import itertools
+neighbourhood = data['neighbourhoods']
+ord_quantity = []
+distance = []
 
-f = open('Input data\level1a.json')
-d = json.load(f)
-neighbour=d['neighbourhoods']
-#let us define two lists to hold order quantity and distances
-ord_quantity=[]
-distance=[]
-for i in neighbour.keys():
-    temp=list(neighbour[i].values())
+for i in neighbourhood.keys():
+    temp = list(neighbourhood[i].values())
     ord_quantity.append(temp[0])
     distance.append(temp[1])
 
-res_dist=d['restaurants']['r0']['neighbourhood_distance']
+res_dist = data['restaurants']['r0']['neighbourhood_distance']
 
-print("Distance to neighbourhood from restaurant r0 :",res_dist)
+print("Distance to neighbourhood from restaurant r0:", res_dist)
+print("Order_quantity for each area:", ord_quantity)
 
-print("Order_quantity for each area ",ord_quantity)
+max_cap = data['vehicles']['v0']['capacity']
 
-max_cap=d['vehicles']['v0']['capacity']
+print("Capacity of scooter:", max_cap)
+print("Total quantity:", sum(ord_quantity))
+print("Minimum number of slots:", round(sum(ord_quantity) / max_cap))
 
-print("capacity of scooter :",max_cap)
 
-print("Total quantity :",sum(ord_quantity))
-
-print("minimum number of slots :",round(sum(ord_quantity)/max_cap))
-
-def find_optimized_slots(distance, res_dist, ord_quantities, max_cap):
+def find_optimized_slots(distances, res_dist, ord_quantities, max_cap):
     node_order = sorted(range(len(res_dist)), key=lambda x: res_dist[x])
     slots = []
     current_slot = []
@@ -46,7 +45,7 @@ def find_optimized_slots(distance, res_dist, ord_quantities, max_cap):
             current_capacity += ord_quantities[node]
 
             if len(current_slot) > 1:
-                current_distance += distance[current_slot[-2]][current_slot[-1]]
+                current_distance += distances[current_slot[-2]][current_slot[-1]]
         else:
             slots.append((current_slot, current_distance))
             current_slot = [node]
@@ -64,7 +63,7 @@ def find_optimized_slots(distance, res_dist, ord_quantities, max_cap):
         best_order = []
 
         for order in possible_orders:
-            dist = sum(distance[order[i]][order[i+1]] for i in range(len(order) - 1))
+            dist = sum(distances[order[i]][order[i + 1]] for i in range(len(order) - 1))
             if dist < min_distance:
                 min_distance = dist
                 best_order = order
@@ -72,19 +71,20 @@ def find_optimized_slots(distance, res_dist, ord_quantities, max_cap):
         optimized_slots.append((best_order, min_distance))
 
     return optimized_slots
-os=find_optimized_slots(distance, res_dist, ord_quantity,max_cap)
-final_lst=[]
-for i in os:
-    temp=[]
-    temp.append('r0')
-    for j in i[0]:
-        temp.append('n'+str(j))
+
+
+optimized_slots = find_optimized_slots(distance, res_dist, ord_quantity, max_cap)
+final_lst = []
+
+for i in optimized_slots:
+    temp = ['r0']
+    temp.extend(['n' + str(j) for j in i[0]])
     temp.append('r0')
     final_lst.append(temp)
 
-final_slots={"v0": {"path1": final_lst[0], "path2": final_lst[1], "path3": final_lst[2],"path4":final_lst[3]}}
+final_slots = {"v0": {f"path{i + 1}": final_lst[i] for i in range(len(final_lst))}}
 print(final_slots)
 
-save_file = open("MyOutput/level1a_output.json", "w")  
-json.dump(final_slots, save_file, indent = 6)  
-save_file.close()
+output_file_path = "MyOutput/level1a_output.json"
+with open(output_file_path, "w") as save_file:
+    json.dump(final_slots, save_file, indent=6)
