8b1a4998195fb97aa87d6d74267f3b8754664d74
rrsr28 <rr.sanjayram@gmail.com>
2024-01-06 12:37:29 +0530

    Attempt 2

--- Level0.py
+++ Level0.py
@@ -9,7 +9,7 @@ import json
 import numpy as np
 
 
-def read_input_data(file_path):
+"""def read_input_data(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
@@ -79,5 +79,48 @@ def main():
     generate_output_json(path)
 
 if __name__ == "__main__":
-    main()
+    main()"""
 # ----------------------------------------------------------
+
+from typing import List
+import math
+
+def nearest_neighbor(cities, graph):
+    unvisited = cities.copy()
+    print(unvisited)
+    current = cities[0]
+    unvisited.remove(current)
+    tour = [current]
+    while unvisited:
+        next_city = min(unvisited, key=lambda city: graph[cities.index(current)][cities.index(city)])
+        tour.append(next_city)
+        unvisited.remove(next_city)
+        current = next_city
+    return tour
+
+file_path = 'Input data/level0.json'
+
+with open(file_path, 'r') as file:
+    data = json.load(file)
+
+path = []
+cost = 0
+
+graph = [[0 for column in range(21)]
+                      for row in range(21)]
+for i in range(0, 20):
+    graph[i+1] = data["neighbourhoods"]["n" + str(i)]["distances"]
+graph[0] = data["restaurants"]["r0"]["neighbourhood_distance"]
+
+cities = ["r0"] + ["n" + str(i) for i in range(20)]
+print(cities)
+path_ = nearest_neighbor(cities, graph)
+print(path_)
+
+for i in path:
+    path_.append('n'+str(i-1))
+path_.append('r0')
+dictionary = {"v0": {"path": path_}}
+json_object = json.dumps(dictionary, indent=4)
+with open("level0_output.json", "w") as outfile:
+    outfile.write(json_object)
